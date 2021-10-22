7533d56ba2a8d961eafef0a889b2ef493f9569a6
Atif Ali <atifshoukatali@yahoo.com>
2021-10-22 15:20:57 -0700

    return empty frame on query

--- pkg/plugin/datasource.go
+++ pkg/plugin/datasource.go
@@ -156,12 +156,7 @@ func (ds *MQTTDatasource) Query(query backend.DataQuery) backend.DataResponse {
 	// ensure the client is subscribed to the topic.
 	ds.Client.Subscribe(qm.Topic)
 
-	messages, ok := ds.Client.Messages(qm.Topic)
-	if !ok {
-		return response
-	}
-
-	frame := ToFrame(qm.Topic, messages)
+	frame := data.NewFrame(qm.Topic)
 
 	if qm.Topic != "" {
 		frame.SetMeta(&data.FrameMeta{
