2c7884b63f0ee97a25d6cdd4ae32b1ec11849ff7
Med951 <abidimed95@gmail.com>
2022-04-21 07:37:21 +0200

    feature: nested json object parsing

--- pkg/plugin/message_test.go
+++ pkg/plugin/message_test.go
@@ -5,13 +5,14 @@ import (
 	"testing"
 	"time"
 
+	"github.com/grafana/grafana-plugin-sdk-go/data"
 	"github.com/grafana/mqtt-datasource/pkg/mqtt"
 	"github.com/grafana/mqtt-datasource/pkg/plugin"
 	"github.com/stretchr/testify/require"
 )
 
 func TestSimpleValueMessage(t *testing.T) {
-	frame := plugin.ToFrame("test/data", []mqtt.Message{
+	frame := plugin.ToFrame(make(map[string]*data.Field), "test/data", []mqtt.Message{
 		{
 			Timestamp: time.Unix(1, 0),
 			Value:     "1",
@@ -24,31 +25,45 @@ func TestSimpleValueMessage(t *testing.T) {
 
 func TestJSONValuesMessage(t *testing.T) {
 	timestamp := time.Unix(1, 0)
-	values := []float64{
-		-0.5182926829268293,
-		-0.3582317073170732,
+	values := []interface{}{
+		"-0.5182926829268293",
+		"Hi",
 		0.1753048780487805,
-		0.20599365234375,
+		false,
 		-0.050048828125,
-		1.03582763671875,
+		true,
 	}
-	msg := fmt.Sprintf(`{"ax": %v, "ay": %v, "az": %v, "gx": %v, "gy": %v, "gz": %v}`,
-		values[0], values[1], values[2], values[3], values[4], values[5])
-	frame := plugin.ToFrame("test/data", []mqtt.Message{
+	cases := []struct {
+		msg  string
+		keys []string
+	}{
 		{
-			Timestamp: timestamp,
-			Value:     msg,
-		},
-	})
-	numFields := len(values) + 1
-	require.NotNil(t, frame)
-	require.Equal(t, numFields, len(frame.Fields))
-	v, ok := frame.Fields[0].ConcreteAt(0)
-	require.Equal(t, true, ok)
-	require.Equal(t, v, timestamp)
-	for idx, val := range values {
-		v, err := frame.Fields[idx+1].FloatAt(0)
-		require.NoError(t, err)
-		require.Equal(t, val, v)
+			msg: fmt.Sprintf(`{"ax": %#v, "ay": %#v, "az": %#v, "gx": %#v, "gy": %#v, "gz": %#v}`,
+				values[0], values[1], values[2], values[3], values[4], values[5]),
+			keys: []string{"ax", "ay", "az", "gx", "gy", "gz"}},
+
+		{
+			msg: fmt.Sprintf(`{"ax": %#v, "ay":{"az": { "gx": %#v}, "gy": %#v}, "gz":{"b":{"bx": %#v,"by": %#v, "bz": %#v}}}`,
+				values[0], values[1], values[2], values[3], values[4], values[5]),
+			keys: []string{"ax", "ay.az.gx", "ay.gy", "gz.b.bx", "gz.b.by", "gz.b.bz"}},
+	}
+	for _, c := range cases {
+		frame := plugin.ToFrame(make(map[string]*data.Field), "test/data", []mqtt.Message{
+			{
+				Timestamp: timestamp,
+				Value:     c.msg,
+			},
+		})
+		numFields := len(values) + 1
+		require.NotNil(t, frame)
+		require.Equal(t, numFields, len(frame.Fields))
+		v, ok := frame.Fields[0].ConcreteAt(0)
+		require.Equal(t, true, ok)
+		require.Equal(t, v, timestamp)
+		for idx, val := range values {
+			require.Equal(t, frame.Fields[idx+1].Name, c.keys[idx])
+			v, _ := frame.Fields[idx+1].ConcreteAt(0)
+			require.Equal(t, val, v)
+		}
 	}
 }
