8a1a03a007fe57ac3777ff86bd14dfe7478e6e82
Niklas Ciecior <niklas.ciecior@tricera.energy>
2024-01-18 13:22:48 +0100

    Add variable support

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,9 +1,18 @@
-import { DataSourceInstanceSettings } from '@grafana/data';
-import { DataSourceWithBackend } from '@grafana/runtime';
+import { DataSourceInstanceSettings, ScopedVars } from '@grafana/data';
+import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
 import { MqttDataSourceOptions, MqttQuery } from './types';
 
 export class DataSource extends DataSourceWithBackend<MqttQuery, MqttDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<MqttDataSourceOptions>) {
     super(instanceSettings);
+  }
+
+  applyTemplateVariables(query: MqttQuery, scopedVars: ScopedVars): Record<string, any> {
+    const resolvedQuery: MqttQuery = {
+      ...query,
+      topic: getTemplateSrv().replace(query.topic, scopedVars),
+    };
+
+    return resolvedQuery;
   }
 }
