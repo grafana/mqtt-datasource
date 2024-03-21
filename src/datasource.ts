5fbc6d113ad8de14f728534eeb1e5c8218f126c9
Niklas <117170532+NiklasCi@users.noreply.github.com>
2024-03-21 16:06:22 +0100

    Add support for variables, "+" and "#" (#95)

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,9 +1,21 @@
-import { DataSourceInstanceSettings } from '@grafana/data';
-import { DataSourceWithBackend } from '@grafana/runtime';
+import { DataSourceInstanceSettings, ScopedVars } from '@grafana/data';
+import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
 import { MqttDataSourceOptions, MqttQuery } from './types';
 
 export class DataSource extends DataSourceWithBackend<MqttQuery, MqttDataSourceOptions> {
   constructor(instanceSettings: DataSourceInstanceSettings<MqttDataSourceOptions>) {
     super(instanceSettings);
+  }
+
+  applyTemplateVariables(query: MqttQuery, scopedVars: ScopedVars): Record<string, any> {
+    let resolvedTopic = getTemplateSrv().replace(query.topic, scopedVars);
+    resolvedTopic = resolvedTopic.replace(/\+/gi, '__PLUS__');
+    resolvedTopic = resolvedTopic.replace(/\#/gi, '__HASH__');
+    const resolvedQuery: MqttQuery = {
+      ...query,
+      topic: resolvedTopic,
+    };
+
+    return resolvedQuery;
   }
 }
