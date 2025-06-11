801c6cfb199ac09cce99741ed61f85f433a06ba9
ismail simsek <ismailsimsek09@gmail.com>
2025-06-11 12:23:16 +0200

    fix e2e

--- tests/e2e/smoke.spec.ts
+++ tests/e2e/smoke.spec.ts
@@ -1,7 +1,8 @@
-import { test, expect } from '@grafana/plugin-e2e';
+import { expect, test } from '@grafana/plugin-e2e';
 
 test('Smoke test: plugin loads', async ({ createDataSourceConfigPage, page }) => {
-  await createDataSourceConfigPage({ type: 'mqtt-datasource' });
+  await createDataSourceConfigPage({ type: 'grafana-mqtt-datasource' });
 
-  await expect(await page.getByPlaceholder('TCP (tcp://), TLS (tls://), or WebSocket (ws://)')).toBeVisible();
+  await expect(await page.getByText('Type: MQTT', { exact: true })).toBeVisible();
+  await expect(await page.getByText('URI *', { exact: true })).toBeVisible();
 });
